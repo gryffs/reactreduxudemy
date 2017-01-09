f6962907d16d03b6d06f434ca617e67a1859ae20
Chad Griffis <cmg123@me.com>
2017-01-09 15:51:32 -0600

    adds placeholder to searchbar and changes background

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
+          placeholder="Search YouTube"
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
       </div>
