ee525a5a3aaf21e3109d187694cf363e6203c5e6
Chad Griffis <cmg123@me.com>
2017-01-11 19:12:19 -0600

    adds a basic working redux container displaying a list of books

---
+++ Redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: "You Don't Know JS" },
+    { title: 'Eloquent JavaScript' },
+    { title: 'The Jungle Book' }
+  ]
+};
