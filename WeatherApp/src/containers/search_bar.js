1ce1c19b6e797264feab6d15150da380c5b0baa0
Chad Griffis <cmg123@me.com>
2017-01-13 11:33:44 -0600

    refactors input-bar to a controled component

--- WeatherApp/src/containers/search_bar.js
+++ WeatherApp/src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor() {
+    super();
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value })
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input 
+          placeholder="Get a five-day forcast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
