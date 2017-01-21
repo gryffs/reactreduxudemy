25217e64950c4bb184fb3d34dba3b5fdc5a6897a
Chad Griffis <cmg123@me.com>
2017-01-21 14:58:05 -0600

    adds additional charts, avg lines, and K to F conversion

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -5,16 +5,19 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const id = cityData.city.id;
+    const temps = cityData.list.map(weather => weather.main.temp).map(temp => temp * (9/5) - 459.67);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
-      <tr key={name}>
+      <tr key={id}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color='orange' />
-        </td>
+        <td><Chart data={temps} color='orange' units='F' /></td>
+        <td><Chart data={pressures} color='green' units='hPa' /></td>
+        <td><Chart data={humidities} color='black' units='%' /></td>
       </tr>
-      );
+    );
   }
 
   render() {
@@ -23,16 +26,16 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
-          {this.props.weather.map(this.renderWeather)}
+            {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
-      )
+    );
   }
 }
 
