1f263ac6489d39551026d8beb657af2e15368289
Chad Griffis <cmg123@me.com>
2017-01-22 16:56:23 -0600

    adds deletePost action creator and set up reroute on delete and routing back in Posts page

--- RBlog/src/actions/index.js
+++ RBlog/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=jfoksnchwekdaldmxnbqowpajfgsaj';
@@ -22,7 +23,7 @@ export function createPost(props) {
   return {
     type: CREATE_POST,
     payload: request
-  }
+  };
 }
 
 export function fetchPost(id) {
@@ -31,5 +32,14 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
-  }
+  };
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
+    payload: request
+  };
 }
